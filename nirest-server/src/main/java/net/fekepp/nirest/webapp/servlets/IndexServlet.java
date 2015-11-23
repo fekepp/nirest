958bd1efcf9ca2e3e38583601ad823389aca4baa
Felix Leif Keppmann <felix.leif@keppmann.de>
2015-11-23 18:19:03 +0100

    Replace index HTML by index servlet

---
+++ nirest-server/src/main/java/net/fekepp/nirest/webapp/servlets/IndexServlet.java
@@ -0,0 +1,44 @@
+package net.fekepp.nirest.webapp.servlets;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author "Felix Leif Keppmann"
+ */
+@Path("/")
+public class IndexServlet {
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public Response html() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(""
+
+				+ "<html>"
+
+				+ "\n\t<head>\n\t\t<title>Natural Interaction via REST (NIREST)</title>\n\t</head>"
+
+				+ "\n\t<body>"
+
+				+ "\n\t\t<h1>Natural Interaction via REST (NIREST)</h1>"
+
+				+ "\n\t\t<h2>Resources:</h2>"
+				
+				+ "\n\t\t<a href=\"device\">device</a>"
+
+				+ "\n\t\t<br/>"
+
+				+ "\n\t\t<a href=\"user\">user</a>"
+
+				+ "\n\t</body>"
+
+				+ "\n</html>");
+
+		return Response.status(200).entity(buffer.toString()).build();
+	}
+
+}
