234de0dafc22a2315078e9d19344bedfc0932619
Felix Leif Keppmann <felix.leif@keppmann.de>
2016-04-26 17:27:46 +0200

    Remove listening to strict host which should fix Docker port mapping

--- nirest-ldfu/src/main/java/net/fekepp/nirest/ldfu/Main.java
+++ nirest-ldfu/src/main/java/net/fekepp/nirest/ldfu/Main.java
@@ -8,7 +8,6 @@ public class Main {
 	private static LdfuController ldfuController = new LdfuController();
 
 	public static void main(String[] args) {
-		ldfuController.setHost("127.0.0.1");
 		ldfuController.setPort(8888);
 		ldfuController.startBlocking();
 	}
