32d9d75e2ccc73f1679d944b77855cbc2566b19b
Felix Leif Keppmann <felix.leif@keppmann.de>
2016-05-01 10:58:51 +0200

    Add example instance, programs, resources, requests of LD-Fu integration

---
+++ examples/watch-shutdown.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+watch "rapper -q -i turtle -o turtle -f 'xmlns:xsd=\"http://www.w3.org/2001/XMLSchema#\"' -f 'xmlns:nirest=\"http://vocab.arvida.de/2014/02/nirest/vocab#\"' -f 'xmlns:scenario=\"http://scenario#\"' http://localhost:8888/scenario/r/shutdown"
